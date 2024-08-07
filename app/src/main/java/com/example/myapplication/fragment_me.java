c2beab84ed96ef44dea1c220aa867f5ba9319afa
ASUS <2605787761@qq.com>
2024-08-07 21:28:38 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/fragment_me.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//我的主页//
+public class fragment_me extends Fragment {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+}
