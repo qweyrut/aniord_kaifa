bbad5c848b1f8979fef25697c98f18f924c3223c
ASUS <2605787761@qq.com>
2024-08-11 19:43:52 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/fragment/interface_two.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class interface_two extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_interface_two, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/fragment/search_for.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class search_for extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search_for, container, false);
+    }
+}
